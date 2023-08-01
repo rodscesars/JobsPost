392cf6a80ce98e1a364c3d51c10f9f38d2fb6f23
Rodrigo César <67443869+rodscesars@users.noreply.github.com>
2023-08-01 15:36:06 -0300

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 export default axios.create({
   baseURL: "https://parseapi.back4app.com/classes",
   headers: {
-    'X-Parse-Application-Id': '0RN8jSSFhy3k0g9ASjdWh5FahKtENT897Ut96BFz',
-    'X-Parse-REST-API-Key': 'b1fqhHoWC32Hvb1RAd3QCAferSgPB0zxV4mWtSgj',
+    'X-Parse-Application-Id': 'dpOffLRBbjdI0tvMzj3NGGYqY0mvKG9hGIZ6dKVP',
+    'X-Parse-REST-API-Key': 'mFXONL1NRqhmcc3N6FQngYhZcFVkIZqivVSOL3R9',
     'Content-Type': 'application/json'
   }
 });
